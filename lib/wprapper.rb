6d51cf6811bd039ad780c5b4e10e9cc7efa85df2
Mihai Bobina <mihai@frombase.com>
2015-09-14 15:23:35 +0300

    spec upload feature image.

--- lib/wprapper.rb
+++ lib/wprapper.rb
@@ -1,4 +1,5 @@
 require 'active_support'
+require 'rest_client'
 require 'wprapper/version'
 require 'wprapper/configuration'
 require 'wprapper/base'
@@ -6,6 +7,7 @@ require 'wprapper/post'
 require 'wprapper/author'
 require 'wprapper/category'
 require 'wprapper/wordpress'
+require 'wprapper/wordpress_json_api'
 
 module Wprapper
   class << self
